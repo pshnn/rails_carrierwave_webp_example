c2895fc9cb13933d42d160ac976ce46c7124d894
Igor <pshnn+g1thu6@protonmail.com>
2019-06-14 01:32:33 +0300

    Create PostImageUploader

---
+++ app/uploaders/post_image_uploader.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Manages uploading of post images
+class PostImageUploader < CarrierWave::Uploader::Base
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+end
