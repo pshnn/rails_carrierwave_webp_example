e3056fe06c120e43344bd1fd464b9f1c68023904
Igor <pshnn+g1thu6@protonmail.com>
2019-06-17 23:42:04 +0300

    Update PostImageUploader
    - include WebPConverter module;
    - add :webp version.

--- app/uploaders/post_image_uploader.rb
+++ app/uploaders/post_image_uploader.rb
@@ -2,7 +2,19 @@
 
 # Manages uploading of post images
 class PostImageUploader < CarrierWave::Uploader::Base
+  include WebPConverter
+
   storage :file
+
+  version :webp do
+    process convert_to_webp: [{ quality: 80, method: 5 }]
+
+    def full_filename(file)
+      return file if file.split('.').last == 'webp'
+
+      file + '.webp'
+    end
+  end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
